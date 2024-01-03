670bd9b4639a6e00a85eb9681afb40df5f61d67f
Parth Nikam <parthsnikam12co@gmail.com>
2024-01-03 12:08:57 +0530

    Implemented blinking detection using eye aspect ratio

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import dlib
+from math import hypot
 
 cap = cv2.VideoCapture(0)
 
@@ -12,6 +13,25 @@ def midpoint(p1, p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+
+def get_blinking_ratio(eye_points, facial_landmarks):
+    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+
+    hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+    ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+
+    hor_line_length = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+    ver_line_length = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+
+    ratio = hor_line_length / ver_line_length
+    return ratio
+
+
 while True:
     _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -20,15 +40,14 @@ while True:
     for face in faces:
         landmarks = predictor(gray, face)
 
-        left_point = (landmarks.part(36).x, landmarks.part(36).y)
-        right_point = (landmarks.part(39).x, landmarks.part(39).y)
+        # Detect blinking
+        left_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        right_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
+        blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
 
-        hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+        if blinking_ratio > 5.7:
+            cv2.putText(frame, "BLINKING", (50, 150), font, 7, (255, 0, 0))
 
-        top_point = midpoint(landmarks.part(37), landmarks.part(38))
-        bottom_point = midpoint(landmarks.part(41), landmarks.part(40))
-
-        ver_line = cv2.line(frame, top_point, bottom_point, (0, 255, 0), 2)
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)
