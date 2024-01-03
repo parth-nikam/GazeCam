a72b908583f18fa7d4558fa640d88cd27e1a5aa5
Parth Nikam <parthsnikam12co@gmail.com>
2024-01-03 11:44:35 +0530

    Added facial landmark detection and lines to visualize eye regions

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import cv2
 import numpy as np
+import dlib
 
 cap = cv2.VideoCapture(0)
 
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+
+def midpoint(p1, p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+
 while True:
     _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    faces = detector(gray)
+    for face in faces:
+        landmarks = predictor(gray, face)
+
+        left_point = (landmarks.part(36).x, landmarks.part(36).y)
+        right_point = (landmarks.part(39).x, landmarks.part(39).y)
+
+        hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+
+        top_point = midpoint(landmarks.part(37), landmarks.part(38))
+        bottom_point = midpoint(landmarks.part(41), landmarks.part(40))
+
+        ver_line = cv2.line(frame, top_point, bottom_point, (0, 255, 0), 2)
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)
