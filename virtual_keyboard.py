508dd9b01383cb1d2a1ae625d1a7a4a6ae92420c
Parth Nikam <parthsnikam12co@gmail.com>
2024-01-03 15:35:05 +0530

    Created initial keyboard layout display and letter rendering

--- virtual_keyboard.py
+++ virtual_keyboard.py
@@ -1,17 +1,68 @@
 import cv2
 import numpy as np
 
-keyboard = np.zeros((1000, 1500, 3), np.uint8)
+keyboard = np.zeros((600, 1000, 3), np.uint8)
+
+keys_set_1 = {0: "Q", 1: "W", 2: "E", 3: "R", 4: "T",
+              5: "A", 6: "S", 7: "D", 8: "F", 9: "G",
+              10: "Z", 11: "X", 12: "C", 13: "V", 14: "<"}
 
 
-def letter(x, y, text, selected):
+def letter(letter_index, text, letter_light):
     # Keys
+    if letter_index == 0:
+        x = 0
+        y = 0
+    elif letter_index == 1:
+        x = 200
+        y = 0
+    elif letter_index == 2:
+        x = 400
+        y = 0
+    elif letter_index == 3:
+        x = 600
+        y = 0
+    elif letter_index == 4:
+        x = 800
+        y = 0
+    elif letter_index == 5:
+        x = 0
+        y = 200
+    elif letter_index == 6:
+        x = 200
+        y = 200
+    elif letter_index == 7:
+        x = 400
+        y = 200
+    elif letter_index == 8:
+        x = 600
+        y = 200
+    elif letter_index == 9:
+        x = 800
+        y = 200
+    elif letter_index == 10:
+        x = 0
+        y = 400
+    elif letter_index == 11:
+        x = 200
+        y = 400
+    elif letter_index == 12:
+        x = 400
+        y = 400
+    elif letter_index == 13:
+        x = 600
+        y = 400
+    elif letter_index == 14:
+        x = 800
+        y = 400
+
+
     width = 200
     height = 200
     th = 3  # thickness
 
-    if selected:
-        cv2.rectangle(keyboard, (x + th, y + th), (x + width - th, y + height - th), (255, 0, 0), -1)
+    if letter_light is True:
+        cv2.rectangle(keyboard, (x + th, y + th), (x + width - th, y + height - th), (255, 255, 255), -1)
     else:
         cv2.rectangle(keyboard, (x + th, y + th), (x + width - th, y + height - th), (255, 0, 0), th)
 
@@ -27,9 +78,12 @@ def letter(x, y, text, selected):
     cv2.putText(keyboard, text, (text_x, text_y), font_letter, font_scale, (255, 0, 0), font_th)
 
 
-letter(0, 0, "A", False)
-letter(200, 0, "B", False)
-letter(400, 0, "C", False)
+for i in range(15):
+    if i == 5:
+        light = True
+    else:
+        light = False
+    letter(i, keys_set_1[i], light)
 
 cv2.imshow("Keyboard", keyboard)
 cv2.waitKey(0)
