162a0155c019718a1e9f189aa8c114fc791fe8e1
Parth Nikam <parthsnikam12co@gmail.com>
2024-01-03 15:00:17 +0530

    Implemented gaze detection and direction indication using eye analysis

--- main.py
+++ main.py
@@ -32,8 +32,53 @@ def get_blinking_ratio(eye_points, facial_landmarks):
     return ratio
 
 
+def get_gaze_ratio(eye_points, facial_landmarks):
+    # Gaze detection
+    eye_region = np.array([(facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y),
+                           (facial_landmarks.part(eye_points[1]).x, facial_landmarks.part(eye_points[1]).y),
+                           (facial_landmarks.part(eye_points[2]).x, facial_landmarks.part(eye_points[2]).y),
+                           (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y),
+                           (facial_landmarks.part(eye_points[4]).x, facial_landmarks.part(eye_points[4]).y),
+                           (facial_landmarks.part(eye_points[5]).x, facial_landmarks.part(eye_points[5]).y)], np.int32)
+
+    # cv2.polylines(frame, [eye_region], True, (0, 0, 255), 2)
+
+    height, width, _ = frame.shape
+    mask = np.zeros((height, width), np.uint8)
+    cv2.polylines(mask, [eye_region], True, 255, 2)
+    cv2.fillPoly(mask, [eye_region], 255)
+    eye = cv2.bitwise_and(gray, gray, mask=mask)
+
+    # cv2.imshow("Eye", eye)
+
+    min_x = np.min(eye_region[:, 0])
+    max_x = np.max(eye_region[:, 0])
+    min_y = np.min(eye_region[:, 1])
+    max_y = np.max(eye_region[:, 1])
+
+    grey_eye = eye[min_y: max_y, min_x: max_x]
+    _, threshold_eye = cv2.threshold(grey_eye, 70, 255, cv2.THRESH_BINARY)
+    height, width = threshold_eye.shape
+
+    left_side_threshold = threshold_eye[0: height, 0: int(width / 2)]
+    left_side_white = cv2.countNonZero(left_side_threshold)
+
+    right_side_threshold = threshold_eye[0: height, int(width / 2): width]
+    right_side_white = cv2.countNonZero(right_side_threshold)
+
+    if left_side_white == 0:
+        gaze_ratio = 1
+    elif right_side_white == 0:
+        gaze_ratio = 5
+    else:
+        gaze_ratio = left_side_white / right_side_white
+
+    return gaze_ratio
+
+
 while True:
     _, frame = cap.read()
+    new_frame = np.zeros((500, 500, 3), np.uint8)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(gray)
@@ -49,40 +94,28 @@ while True:
             cv2.putText(frame, "BLINKING", (50, 150), font, 7, (255, 0, 0))
 
         # Gaze detection
+        gaze_ratio_left_eye = get_gaze_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        gaze_ratio_right_eye = get_gaze_ratio([42, 43, 44, 45, 46, 47], landmarks)
+        gaze_ratio = (gaze_ratio_left_eye + gaze_ratio_right_eye) / 2
 
-        left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
-                                    (landmarks.part(37).x, landmarks.part(37).y),
-                                    (landmarks.part(38).x, landmarks.part(38).y),
-                                    (landmarks.part(39).x, landmarks.part(39).y),
-                                    (landmarks.part(40).x, landmarks.part(40).y),
-                                    (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
+        if gaze_ratio <= 0.7:
+            cv2.putText(frame, "RIGHT", (50, 100), font, 2, (0, 0, 255), 2)
+        elif 0.7 < gaze_ratio < 1.5:
+            cv2.putText(frame, "CENTER", (50, 100), font, 2, (0, 0, 255), 2)
+        else:
+            cv2.putText(frame, "LEFT", (50, 100), font, 2, (0, 0, 255), 2)
 
-        # cv2.polylines(frame, [left_eye_region], True, (0, 0, 255), 2)
+        # Showing Direction
 
-        height, width, _ = frame.shape
-        mask = np.zeros((height, width), np.uint8)
-
-        cv2.polylines(mask, [left_eye_region], True, 255, 2)
-        cv2.fillPoly(mask, [left_eye_region], 255)
-
-        left_eye = cv2.bitwise_and(gray, gray, mask=mask)
-
-        min_x = np.min(left_eye_region[:, 0])
-        max_x = np.max(left_eye_region[:, 0])
-
-        min_y = np.min(left_eye_region[:, 1])
-        max_y = np.max(left_eye_region[:, 1])
-
-        grey_eye = left_eye[min_y: max_y, min_x: max_x]
-        _, threshold_eye = cv2.threshold(grey_eye, 70, 255, cv2.THRESH_BINARY)
-
-        eye = cv2.resize(grey_eye, None, fx=5, fy=5)
-        threshold_eye = cv2.resize(threshold_eye, None, fx=5, fy=5)
-        cv2.imshow("Eye", eye)
-        cv2.imshow("Threshold", threshold_eye)
-        cv2.imshow("Left eye", left_eye)
+        if gaze_ratio <= 0.7:
+            new_frame[:] = (0, 0, 255)
+        elif 0.7 < gaze_ratio < 1.5:
+            new_frame[:] = (0, 255, 0)
+        else:
+            new_frame[:] = (255, 0, 0)
 
     cv2.imshow("Frame", frame)
+    cv2.imshow("New frame", new_frame)
 
     key = cv2.waitKey(1)
     if key == 27:
